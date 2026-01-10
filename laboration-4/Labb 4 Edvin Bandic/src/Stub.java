1182c5b6a14e95de0a6e66cb0e87d5640c719031
edvin.bandic@gmail.com <edvin.bandic@gmail.com>
2026-01-10 16:51:12 +0100

    två grejer kvar
    Put och image

--- laboration-4/Labb 4 Edvin Bandic/src/Stub.java
+++ laboration-4/Labb 4 Edvin Bandic/src/Stub.java
@@ -10,15 +10,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.FindIterable;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import java.util.List;
 
 public class Stub {
 
@@ -51,7 +56,7 @@ public class Stub {
 
         MongoDatabase database = mongoClient.getDatabase(dbName);
 
-        myCollection = database.getCollection("restaurants");
+        myCollection = database.getCollection("movies");
 
         logger.info("Connected to MongoDB, using database '{}'", dbName);
     }
@@ -59,27 +64,233 @@ public class Stub {
     private static void startServer() {
         Javalin app = Javalin.create().start(4567);
 
-        app.get("/foo/{name}", Stub::getFooByName);
+        app.get("/title/{title}", Stub::getTitleByTitle);
+        app.get("/fullplot/{title}" , Stub::getFullplotByTitle);
+        app.get("/cast/{title}" , Stub::getCastByTitle);
+        app.get("/genre/{genre}" , Stub::getGenreByGenre);
+        app.get("/actor/{actor}" , Stub::getActorByActor);
+        app.get("/similar/{title}", Stub::getSimilarMoviesByTitle);
+        
+        app.post("/title", Stub::createMovie);
+        
+        app.delete("/title/{title}", Stub::deleteMovie);
        
     }
 
-    private static void getFooByName(Context ctx) {
-        String inputName = ctx.pathParam("name");
-        String name = capitalizeFully(inputName.toLowerCase(Locale.ROOT));
+    private static void getTitleByTitle(Context ctx) {
+        String inputName = ctx.pathParam("title");
+        String title = capitalizeFully(inputName.toLowerCase(Locale.ROOT));
 
-        Document doc = myCollection.find(eq("name", name)).first();
+        Document doc = myCollection.find(eq("title", title)).first();
 
         if (doc != null) {
             doc.remove("_id");
+            doc.remove("poster");
+            doc.remove("cast");
+            doc.remove("fullplot");
+            JsonObject response = JsonParser.parseString(doc.toJson()).getAsJsonObject();
             ctx.status(200);
             ctx.contentType("application/json");
-            ctx.result(doc.toJson());
+            ctx.result(response.toString());
         } else {
             ctx.status(404);
             ctx.contentType("application/json");
-            ctx.result(jsonError("Foo not found.").toString());
+            ctx.result(jsonError("Title not found.").toString());
         }
     }
+    
+    private static void getFullplotByTitle(Context ctx) {
+        String inputName = ctx.pathParam("title");
+        String title = capitalizeFully(inputName.toLowerCase(Locale.ROOT));
+
+        Document doc = myCollection.find(eq("title", title)).projection(new Document("title", 1).append("fullplot", 1).append("_id", 0)).first();
+
+        if (doc != null) {
+            JsonObject response = JsonParser.parseString(doc.toJson()).getAsJsonObject();
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(response.toString());
+        } else {
+            ctx.status(404);
+            ctx.contentType("application/json");
+            ctx.result(jsonError("Title not found.").toString());
+        }
+    }
+    
+    private static void getCastByTitle(Context ctx) {
+        String inputName = ctx.pathParam("title");
+        String title = capitalizeFully(inputName.toLowerCase(Locale.ROOT));
+
+        Document doc = myCollection.find(eq("title", title)).projection(new Document("title", 1).append("cast", 1).append("_id", 0)).first();
+
+        if (doc != null) {
+            JsonObject response = JsonParser.parseString(doc.toJson()).getAsJsonObject();
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(response.toString());
+        } else {
+            ctx.status(404);
+            ctx.contentType("application/json");
+            ctx.result(jsonError("Title not found.").toString());
+        }
+    }
+    
+    
+    private static void getGenreByGenre(Context ctx) {
+        String inputName = ctx.pathParam("genre");
+        String genre = capitalizeFully(inputName.toLowerCase(Locale.ROOT));
+        
+        int limit = ctx.queryParamAsClass("limit", Integer.class).getOrDefault(10);
+
+
+		FindIterable<Document> results = myCollection.find(eq("genres", genre))
+				.limit(limit);
+		
+
+		JsonArray array = new JsonArray();
+		
+		for (Document doc : results) {
+			 doc.remove("_id");
+             doc.remove("poster");
+             doc.remove("cast");
+             doc.remove("fullplot");
+             
+			array.add(JsonParser.parseString(doc.toJson()).getAsJsonObject());
+		}
+		
+		if(array.isEmpty()){
+			ctx.status(404);
+	        ctx.contentType("application/json");
+	        ctx.result(jsonError("Genre not found.").toString());
+	        return;
+		}
+		
+		ctx.status(200);
+        ctx.contentType("application/json");
+        ctx.result(array.toString());
+
+        }
+    
+    
+    private static void getActorByActor(Context ctx) {
+        String inputName = ctx.pathParam("actor");
+        String actor = capitalizeFully(inputName.toLowerCase(Locale.ROOT));
+        
+        int limit = ctx.queryParamAsClass("limit", Integer.class).getOrDefault(10);
+
+		FindIterable<Document> results = myCollection.find(eq("cast", actor))
+				.limit(limit);
+		
+
+		JsonArray array = new JsonArray();
+		
+		for (Document doc : results) {
+			 doc.remove("_id");
+             
+			array.add(JsonParser.parseString(doc.toJson()).getAsJsonObject());
+		}
+		
+		if(array.isEmpty()){
+			ctx.status(404);
+	        ctx.contentType("application/json");
+	        ctx.result(jsonError("Actor not found.").toString());
+	        return;
+		}
+		
+		ctx.status(200);
+        ctx.contentType("application/json");
+        ctx.result(array.toString());
+
+        }
+    
+    
+	private static void createMovie(Context ctx) {
+		try {
+			String requestBody = ctx.body();
+			Document newDoc = Document.parse(requestBody);
+
+			myCollection.insertOne(newDoc);
+
+			ctx.status(202).contentType("application/json").result();
+
+		} catch (Exception e) {
+			logger.error("Insert failed: {}", e.getMessage());
+			ctx.status(500).contentType("application/json")
+					.result(jsonError("Failed to insert movie.").toString());
+		}
+	}
+	
+	private static void deleteMovie(Context ctx) {
+		try {
+			String inputName = ctx.pathParam("title");
+	        String title = capitalizeFully(inputName.toLowerCase(Locale.ROOT));
+
+			myCollection.deleteOne(eq("title", title));
+
+			ctx.status(204).contentType("application/json").result();
+
+		} catch (Exception e) {
+			logger.error("Insert failed: {}", e.getMessage());
+			ctx.status(409).contentType("application/json")
+					.result(jsonError("Failed to delete movie.").toString());
+		}
+	}
+    
+    
+	private static void getSimilarMoviesByTitle(Context ctx) {
+		String inputCuisine = ctx.pathParam("title");
+		String title = capitalizeFully(inputCuisine.toLowerCase(Locale.ROOT));
+
+		int limit = ctx.queryParamAsClass("limit", Integer.class).getOrDefault(10);
+		
+		Document movie = myCollection.find(eq("title", title)).first();
+		
+		
+		if (movie == null) {
+	        ctx.status(404).json(jsonError("Title not found"));
+	        return;
+	    }
+		
+		
+		List<String> genres = movie.getList("genres", String.class);
+		
+		if (genres == null || genres.isEmpty()) {
+	        ctx.status(404).json(jsonError("No genres found"));
+	        return;
+	    }
+		
+		String genre = genres.get(0);
+		
+		
+		FindIterable<Document> results = myCollection.find(eq("genres", genre))
+				.limit(limit);
+
+		JsonArray array = new JsonArray();
+
+		for (Document doc : results) {
+			doc.remove("_id");
+			doc.remove("poster");
+	        doc.remove("cast");
+	        doc.remove("fullplot");
+			array.add(JsonParser.parseString(doc.toJson()).getAsJsonObject());
+		}
+
+		JsonObject response = new JsonObject();
+		response.add("movies", array);
+		
+		if(array.isEmpty()){
+			ctx.status(404);
+	        ctx.contentType("application/json");
+	        ctx.result(jsonError("Similar Movies not found.").toString());
+	        return;
+		}
+
+		ctx.status(200).contentType("application/json").result(response.toString());
+	}
+    
+    
+    
+    
 
   
     private static String capitalizeFully(String input) {
